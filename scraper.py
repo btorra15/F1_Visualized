42bb48c51f079d3e0491a0431b656d2d21c79dca
Brett Torra <bmtorr24@colby.edu>
2022-03-10 22:19:58 -0500

    scraper can now get each race from 2021

--- scraper.py
+++ scraper.py
@@ -1,9 +1,30 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = "https://www.formula1.com/en/results.html/2021/races/1102/united-states/race-result.html"
+URL = "https://www.formula1.com/en/results.html/2021/races.html"
 page = requests.get(URL)
 soup = BeautifulSoup(page.content, "html.parser")
+
+drivers_raceresult = []
+drivers_fastestlap = []
+drivers_pitstopsummary = []
+drivers_startinggrid = []
+drivers_qualifying = []
+drivers_practice3 = []
+drivers_practice2 = []
+drivers_practice1 = []
+
+races = []
+
+race_links = []  # holds the link to each race
+
+
+filters = soup.find_all('ul', {'class': 'resultsarchive-filter ResultFilterScrollable'}) # grabs each list of filters (race, filters on drivers, year)
+
+races_html = filters[2]  # grabs the html of the races filter
+
+for race in races_html:
+    races.append(race.text.strip())
 
 
 # finds the table which contains data
@@ -11,7 +32,6 @@ results = soup.find('table', {'class': 'resultsarchive-table'})
 
 # finds list of drivers who drove in race
 drivers_html = soup.find_all('tr')
-drivers = []  # holds list of drivers
 
 # strips HTML from each driver and appends to driver list
 for driver in drivers_html:
@@ -19,7 +39,4 @@ for driver in drivers_html:
     driver_lastname = driver.find('span', {'class': 'hide-for-mobile'})
 
     if(driver_firstname != None):
-        drivers.append(driver_firstname.text + ' ' + driver_lastname.text)
-
-for i in range(len(drivers)):
-    print(drivers[i])
+        drivers_raceresult.append(driver_firstname.text + ' ' + driver_lastname.text)
