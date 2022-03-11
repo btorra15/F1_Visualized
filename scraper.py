1027ed51f746c45f52758af276c39163b9d1033d
Brett Torra <bmtorr24@colby.edu>
2022-03-11 08:49:37 -0500

    scraper now loops through each race of 2021, and gets each race result

--- scraper.py
+++ scraper.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = "https://www.formula1.com/en/results.html/2021/races.html"
+URL = "https://www.formula1.com//en/results.html/2021/races.html"
 page = requests.get(URL)
 soup = BeautifulSoup(page.content, "html.parser")
 
@@ -19,24 +19,46 @@ races = []
 race_links = []  # holds the link to each race
 
 
+
+
 filters = soup.find_all('ul', {'class': 'resultsarchive-filter ResultFilterScrollable'}) # grabs each list of filters (race, filters on drivers, year)
 
 races_html = filters[2]  # grabs the html of the races filter
 
-for race in races_html:
-    races.append(race.text.strip())
+index = 0
+for race in races_html: # adds each race to a list
+
+    if index % 2 != 0: # gets ride of blank space being added to races
+        races.append(race.text.strip())
+
+    index = index + 1
+
+# adds the links of each race to a list
+links = races_html.find_all("a")
+for link in links:
+    race_links.append(link["href"])
 
 
-# finds the table which contains data
-results = soup.find('table', {'class': 'resultsarchive-table'})
+# loop through each race
+for i in range(1, len(race_links)):
 
-# finds list of drivers who drove in race
-drivers_html = soup.find_all('tr')
+    # get URL of each race
+    URL = "https://www.formula1.com" + race_links[i]
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, "html.parser")
 
-# strips HTML from each driver and appends to driver list
-for driver in drivers_html:
-    driver_firstname = driver.find('span', {'class': 'hide-for-tablet'})
-    driver_lastname = driver.find('span', {'class': 'hide-for-mobile'})
+    # finds the table which contains data
+    results = soup.find('table', {'class': 'resultsarchive-table'})
 
-    if(driver_firstname != None):
-        drivers_raceresult.append(driver_firstname.text + ' ' + driver_lastname.text)
+    # finds list of drivers who drove in race
+    drivers_html = soup.find_all('tr')
+
+    # strips HTML from each driver and appends to driver list
+    for driver in drivers_html:
+        driver_firstname = driver.find('span', {'class': 'hide-for-tablet'})
+        driver_lastname = driver.find('span', {'class': 'hide-for-mobile'})
+
+        if(driver_firstname != None):
+            drivers_raceresult.append(driver_firstname.text + ' ' + driver_lastname.text)
+
+    drivers_raceresult = []
